373799f93ea2d883877ea33eb9a5a518c3fa4588
KVNSPTMN <kevin.spathmann@gmail.com>
2022-05-13 09:46:40 +0200

    finalized script and scraped data

--- main.py
+++ main.py
@@ -1,43 +1,93 @@
+"""
+Web-Scraping Script to collect any match-duration related data of the WWE weekly TV shows RAW, Smackdown, and NXT
+from http://www.profightdb.com.
+The data will cover from the year 2019 until the 10th of May 2022, to compare some time for the COVID-19 pandemic,
+the pandemic itself, and a few months after WWE could have returned to live audience shows in mid 2021.
+"""
 
-import requests, bs4
+import requests, bs4, calendar, sqlite3, time
 
+##------------------------------SQLITE DATABASE CONNECTION------------------------------##
+conn = sqlite3.connect('WWEmatchDuration.db')
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS MatchDurations (date TEXT, brand TEXT, wrestler_s_1 TEXT, wrestler_s_2 TEXT, match_duration_in_sec INTEGER)")
+
+##------------------------------WEBSITE CONNECTION------------------------------##
 PROFIGHTCB_MAIN_URL = "http://www.profightdb.com/cards/wwe/monday-night-raw-"
-PROFIGHTCB_URL_SUFFIX = "43395"
+PROFIGHTCB_URL_SUFFIX = 43402 # 43402 -> NXT from 10th of May 2022
+while PROFIGHTCB_URL_SUFFIX > 28675:
 
-response = requests.get(f"{PROFIGHTCB_MAIN_URL}{PROFIGHTCB_URL_SUFFIX}.html")
+    response = requests.get(f"{PROFIGHTCB_MAIN_URL}{str(PROFIGHTCB_URL_SUFFIX)}.html")
+    webpage = response.text
 
-webpage = response.text
+    ##------------------------------BEAUTIFUL SOUP------------------------------##
+    soup = bs4.BeautifulSoup(webpage, "html.parser")
+    xa=u'\xa0'
 
-soup = bs4.BeautifulSoup(webpage, "html.parser")
+    brand = soup.select_one("div .right-content h1").getText().strip()
+    raw_date = soup.select_one("div .right-content table td").getText()
+    date = raw_date[raw_date.find(",")+2:]
+    date = date.split()
+    year = date[2]
+    month = f"{int(list(calendar.month_abbr).index(date[0])):02d}"
+    day = f"{int(date[1][:-2]):02d}"
+    date = f"{year}-{month}-{day}"
 
-brand = soup.select_one("div .right-content h1").getText().strip()
-card_table = soup.find(name="div", class_="table-wrapper")
+    card_table = soup.find(name="div", class_="table-wrapper")
 
-card_table_rows = card_table.find_all(name="tr")
-# print(card_table_rows)
+    card_table_rows = card_table.find_all(name="tr")
 
-matches = []
+    if ("Monday Night Raw" in brand or "Smackdown" in brand or "NXT TV" in brand) and int(year) >= 2019:
 
-if brand.startswith("WWE Monday Night Raw") or brand.startswith("WWE Friday Night Smackdown") or brand.startswith("NXT TV"):
-    for row in card_table_rows:
-        match = {}
-        if row.find(name="td", width="22%") != None:
-            rows = row.find_all(name="td", width="22%")
-            match = {
-                "competitor_1" : rows[0].getText(),
-                "competitor_2" : rows[1].getText()
-            }
-        all_tds = row.find_all(name="td")
-        for td in all_tds:
-            if len(td.getText()) > 1 and td.getText()[0].isdecimal():
-                # print(td.getText())
-                match["match_length"] = td.getText()
+        if "Raw" in brand:
+            brand = "RAW"
+        elif "Smackdown" in brand:
+            brand = "Smackdown"
+        elif "NXT" in brand:
+            brand = "NXT"
 
-        if len(match) > 1:
-            if match.get("match_length") == None:
-                match["match_length"] = "00:00"
-            matches.append(match)
+        for row in card_table_rows:
+            match = {}
+            if row.find(name="td", width="22%") != None:
+                rows = row.find_all(name="td", width="22%")
+                wrestler_s_1 = rows[0].getText()
+                wrestler_s_2 = rows[1].getText()
+                if xa in wrestler_s_1:
+                    wrestler_s_1 = wrestler_s_1.replace('\xa0', '')
+                    if '(c)' in wrestler_s_1:
+                        wrestler_s_1 = wrestler_s_1.replace('(c)', '')
+                if "'" in wrestler_s_1:
+                    wrestler_s_1 = wrestler_s_1.replace("'", '')
+                if xa in wrestler_s_2:
+                    wrestler_s_2 = wrestler_s_2.replace('\xa0', '')
+                if "'" in wrestler_s_2:
+                    wrestler_s_2 = wrestler_s_2.replace("'", '')
+                match = {
+                    "date" : date,
+                    "brand" : brand,
+                    "wrestler(s)_1" : wrestler_s_1,
+                    "wrestler(s)_2" : wrestler_s_2
+                }
+            all_tds = row.find_all(name="td")
+            for td in all_tds:
+                if len(td.getText()) > 1 and ":" in td.getText() and td.getText()[:1].isdecimal():
+                    # print(td.getText())
+                    match_duration = td.getText()
+                    match_duration_in_sec = (int(match_duration[:2]) * 60) + int(match_duration[3:])
+                    match["match_duration_in_sec"] = match_duration_in_sec
 
-    print(brand)
-    print(matches)
+            if len(match) > 1:
+                if match.get("match_duration_in_sec") == None:
+                    match["match_duration_in_sec"] = "0"
+
+                sql = f"INSERT INTO MatchDurations VALUES ('{str(match['date'])}', '{str(match['brand'])}', '{str(match['wrestler(s)_1'])}', '{str(match['wrestler(s)_2'])}', '{str(match['match_duration_in_sec'])}')"
+                c.execute(sql)
+                conn.commit()
+                print(f"A {match['brand']} match from {date} has been added to the DB.")
+                time.sleep(0.5)
+
+    PROFIGHTCB_URL_SUFFIX -= 1
+    print(PROFIGHTCB_URL_SUFFIX)
+    time.sleep(1)
+
 
